a1fe019dcd59a8dc29af31655158c3fcb513601d
Muhamad Kosih <muhamad.kosih@equity.id>
2023-12-28 15:28:08 +0700

    change vercell command

--- jenkins/scripts/vercel.sh
+++ jenkins/scripts/vercel.sh
@@ -3,7 +3,7 @@
 # Pengecekan apakah Vercel CLI sudah terinstal
 if ! command -v vercel &> /dev/null; then
     echo "Vercel CLI belum terinstal. Melakukan instalasi..."
-    npm install -g vercel
+    npm install  vercel
 else
     echo "Vercel CLI sudah terinstal."
 fi
