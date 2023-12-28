112e253ec972a4509db50ebe70d67d6f7fd35eba
Muhamad Kosih <muhamad.kosih@equity.id>
2023-12-28 16:40:27 +0700

    remove sudo

--- jenkins/scripts/vercel.sh
+++ jenkins/scripts/vercel.sh
@@ -3,7 +3,7 @@
 # Pengecekan apakah Vercel CLI sudah terinstal
 if ! command -v vercel &> /dev/null; then
     echo "Vercel CLI belum terinstal. Melakukan instalasi..."
-    sudo npm install -g vercel
+    npm install -g vercel
 else
     echo "Vercel CLI sudah terinstal."
 fi
